0155343a994b0fe1fe41bfa50049d2750daf1a61
Patrice Tisserand <p.tisserand@gmail.com>
2020-11-18 22:36:09 +0100

    web: disable signup

--- web/auth.py
+++ web/auth.py
@@ -23,39 +23,11 @@ def login_post():
 
     # if the above check passes, then we know the user has the right credentials
     login_user(user, remember=remember)
-    return redirect(url_for('main.profile'))
+    return redirect(url_for('main.index'))
 
 @auth.route('/login')
 def login():
     return render_template('login.html')
-
-
-@auth.route('/signup', methods=['POST'])
-def signup_post():
-    email = request.form.get('email')
-    name = request.form.get('name')
-    password = request.form.get('password')
-
-    # if this returns a user, then the email already exists in database
-    user = User.query.filter_by(email=email).first()
-
-    if user:  # if a user is found, we want to redirect back to signup page so user can try again
-        flash('Email address already exists')
-        return redirect(url_for('auth.signup'))
-
-    # create a new user with the form data. Hash the password so the plaintext version isn't saved.
-    new_user = User(email=email, name=name,
-                    password=generate_password_hash(password, method='sha256'))
-
-    # add the new user to the database
-    db.session.add(new_user)
-    db.session.commit()
-    return redirect(url_for('auth.login'))
-
-
-@auth.route('/signup')
-def signup():
-    return render_template('signup.html')
 
 
 @auth.route('/logout')
