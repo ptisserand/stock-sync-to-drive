01a88dbbcbfc73c8fb6f733de199206c0ad28c5d
Patrice Tisserand <p.tisserand@gmail.com>
2020-11-18 22:36:48 +0100

    web: added drive syncing

--- web/__init__.py
+++ web/__init__.py
@@ -1,11 +1,40 @@
+import os
+import pickle
+from configparser import ConfigParser
+
 from flask import Flask
 from flask_login import LoginManager
 
+main_dir = os.path.dirname(os.path.dirname(__file__))
+
+def retrieve_configuration():
+    creds = None
+    with open(f'{main_dir}/token.pickle', 'rb') as token:
+        creds = pickle.load(token)
+    # The ID and range of a sample spreadsheet (retrieve from config)
+    parser = ConfigParser()
+    parser.read(f'{main_dir}/config.ini')
+
+    drive = {}
+    stock = {}
+    for kk in ['ID_title', 'stock_title', 'price_title']:
+        drive[kk] = parser.get('drive', kk)
+        stock[kk] = parser.get('stock', kk)
+    drive['sheetId'] = parser.get('drive', 'spreadsheet')
+    drive['sheetLabel'] = parser.get('drive', 'sheet_label')
+    drive['quantity_price_title'] = parser.get('drive', 'quantity_price_title')
+    drive['cond_title'] = parser.get('drive', 'cond_title')
+    return drive, stock, creds
+
 def create_app():
     app = Flask(__name__)
-    
+    drive, stock, creds =  retrieve_configuration()
     app.config['SECRET_KEY'] = 'just-another-secret-here'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+    app.config['ROB_DRIVE'] = drive
+    app.config['ROB_STOCK'] = stock
+    app.config['ROB_CREDS'] = creds
+
     from .models import db
     db.init_app(app)
 
--- web/main.py
+++ web/main.py
@@ -1,14 +1,73 @@
-from flask import Blueprint, render_template
-from flask_login import login_required, current_user
+from stock_syncer import StockSyncer
+import os
+from random import randrange
+
+from flask import (
+    Blueprint, render_template, current_app, 
+    request,
+    jsonify
+)
+from flask_login import (
+    login_required, current_user
+)
+
+from google.auth.transport.requests import Request
+
+# XXX path hack
+import sys
+main_dir = os.path.dirname(os.path.dirname(__file__))
+sys.path.append(main_dir)
+
+FAILED = [
+    "https://i.giphy.com/media/MVgLEacpr9KVK172Ne/giphy.webp",
+    "https://i.giphy.com/media/RJaUOmpBQAoE4RuWnj/source.gif"
+]
+
+OK = [
+    "https://i.giphy.com/media/Q732zaivQ5y9aECIPd/giphy.webp",
+    "https://i.giphy.com/media/HX3lSnGXZnaWk/giphy.webp"
+]
+
+
+def get_failed():
+    id = randrange(0, len(FAILED))
+    return FAILED[id]
+
+
+def get_ok():
+    id = randrange(0, len(OK))
+    return OK[id]
+
 
 main = Blueprint('main', __name__)
 
+
 @main.route('/')
-def index():
-    return render_template('index.html')
-
-@main.route('/profile')
 @login_required
-def profile():
-    return render_template('profile.html', name=current_user.name)
+def index():
+    return current_app.send_static_file("html/upload.html")
 
+
+@main.route("/upload", methods=["POST"])
+@login_required
+def upload_xls():
+    print("Upload called")
+    drive = current_app.config['ROB_DRIVE']
+    stock = current_app.config['ROB_STOCK']
+    creds = current_app.config['ROB_CREDS']    
+    if request.files:
+        try:
+            xls = request.files["xls"]
+            xls_data = xls.read()
+            # refresh creds is needed
+            if not creds.valid:
+                if creds.expired and creds.refresh_token:
+                    creds.refresh(Request())
+                else:
+                    return jsonify({"image": get_failed(), "error": "Invalid credentials"}), 500
+            stockSyncer = StockSyncer(drive=drive, stock=stock, credentials=creds)
+            result = stockSyncer.sync(xls_data)
+        except Exception as e:
+            return jsonify({"image": get_failed(), "error": str(e)}), 500
+        return jsonify({"image": get_ok()})
+    return jsonify({"image": get_failed(), "error": "Missing file"}), 403
