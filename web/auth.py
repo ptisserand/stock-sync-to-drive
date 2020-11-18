83c3fa1d963140d91bad072231ead211ce410094
Patrice Tisserand <p.tisserand@gmail.com>
2020-11-18 22:55:13 +0100

    web/auth: remvoed no more needed import

--- web/auth.py
+++ web/auth.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash
 from flask_login import login_user, logout_user, login_required
 
-from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.security import check_password_hash
 from .models import User
 from .models import db
 
