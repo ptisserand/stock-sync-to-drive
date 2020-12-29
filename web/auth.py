3bacf3799e71ad2342c1dacc10dd1691904179c9
Patrice Tisserand <p.tisserand@gmail.com>
2020-12-30 00:31:07 +0100

    web/auth: added api/login route

--- web/auth.py
+++ web/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, redirect, url_for, request, flash
+from flask import Blueprint, render_template, redirect, url_for, request, flash, jsonify
 from flask_login import login_user, logout_user, login_required
 
 from werkzeug.security import check_password_hash
@@ -10,23 +10,40 @@ logger = logging.getLogger("auth")
 auth = Blueprint('auth', __name__)
 
 
+def check_login(email, password, remember=False) -> bool:
+    user = User.query.filter_by(email=email).first()
+    if not user or not check_password_hash(user.password, password):
+        return False
+    else:
+        login_user(user, remember=remember)
+        return True
+
+@auth.route('/api/login', methods=['POST'])
+def login_api():
+    payload = request.get_json()
+    email = payload.get('email', None)
+    password = payload.get('password', None)
+    is_ok = check_login(email, password)
+    if not is_ok:
+        response = jsonify({'error': 'wrong username or password'})
+        return response, 401
+    else:
+        return jsonify({'message': 'welcome!'})
+
 @auth.route('/login', methods=['POST'])
 def login_post():
     email = request.form.get('email')
     password = request.form.get('password')    
     remember = True if request.form.get('remember') else False
 
-    user = User.query.filter_by(email=email).first()
-    # check if the user actually exists
-    # take the user-supplied password, hash it, and compare it to the hashed password in the database
-    if not user or not check_password_hash(user.password, password):
+    is_ok = check_login(email, password, remember)
+    if not is_ok:
         flash('Please check your login details and try again.')
         logger.warning(f"Connection failed for {email}")
         return redirect(url_for('auth.login')) # if the user doesn't exist or password is wrong, reload the page
-
-    # if the above check passes, then we know the user has the right credentials
-    login_user(user, remember=remember)
-    return redirect(url_for('main.index'))
+    else:
+        # if the above check passes, then we know the user has the right credentials
+        return redirect(url_for('main.index'))
 
 @auth.route('/login')
 def login():
